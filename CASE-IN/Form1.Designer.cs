13468de7cf5e8a7d4c4e97e382418c1e92e1ccd0
xomyachok_shaolin <xomyackok8@gmail.com>
2020-04-12 07:22:35 +0500

    Добавьте файлы проекта.

---
+++ CASE-IN/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace CASE_IN {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnExecuteScript = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnExecuteScript
+            // 
+            this.btnExecuteScript.Location = new System.Drawing.Point(297, 38);
+            this.btnExecuteScript.Name = "btnExecuteScript";
+            this.btnExecuteScript.Size = new System.Drawing.Size(75, 23);
+            this.btnExecuteScript.TabIndex = 0;
+            this.btnExecuteScript.Text = "Выполнить";
+            this.btnExecuteScript.UseVisualStyleBackColor = true;
+            this.btnExecuteScript.Click += new System.EventHandler(this.btnExecuteScript_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(46, 38);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.btnExecuteScript);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecuteScript;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
+
---
+++ CASE-IN/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CASE_IN {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnExecuteScript_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Set the MinDate and MaxDate.
+            dateTimePicker.MinDate = new DateTime(2015,01,01, 06, 00, 00);
+            dateTimePicker.MaxDate = new DateTime(2016, 01, 01, 06, 00, 00);
+        }
+    }
+}
